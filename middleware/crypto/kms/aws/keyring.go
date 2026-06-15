da1cca1b1ff7d60b85c820feb981c58ab35d24ea
karagatandev <alex@karagatan.com>
2026-06-14 22:43:05 -0700

    change the license

--- middleware/crypto/kms/aws/keyring.go
+++ middleware/crypto/kms/aws/keyring.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2025 Karagatan LLC.
- * SPDX-License-Identifier: BUSL-1.1
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 // Package awskms implements cryptostore.Keyring backed by AWS KMS envelope
